12b6eb716d2bac64c69c9cc951f8295406558576
gupta, Vagish <Vagishgupta22@gmail.com>
2023-12-21 00:46:14 +0530

    Fix

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@
 const Home = async () => {
 
   return (
-    <div className="w-full mx-auto">    
+    <div className="w-full mx-auto">
       <img className="object-scale-down max-h-full drop-shadow-md rounded-md m-auto" src="/images/banner.jpg" alt="" />
     </div>
   )
